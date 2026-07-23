d55958ab341ded67074caedc99354013eb8f76da
SOWJANYA VANGALA <23MH1A4267@acoe.edu.in>
2026-07-23 06:35:01 +0530

    Create Insertion_Sort.py

---
+++ Sorting/Insertion_Sort.py
@@ -0,0 +1,9 @@
+arr = [64, 25, 12, 22, 11]
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j + 1] = key    
+print(arr)
